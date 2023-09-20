b6ff52f56489d2de09f03db7059602a3e0845d00
Dio <dieudonneiyabivuze@gmail.com>
2023-09-21 01:54:07 +0530

    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+import background from './assets/ny.jpg'
 
 function App() {
+
+  const [quotes, setQuote] = useState([])
+
+  // useEffect react hook to render the API when the page loads
+
+  useEffect(() => {
+    getQuotes()
+
+  },[])
+
+  // Function to fetch the API using axios library
+
+  const getQuotes = () => {
+
+    axios.get('https://api.adviceslip.com/advice')
+           .then((response) => {
+              setQuote([response.data])
+           })
+           .catch((error) => {
+              console.log(error)
+           })
+        }
+
+
+  // UI
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+      backgroundImage : `url(${background})`
+    }}>
+
+      {quotes.map((quote, key) => (
+        <div className='adviceContainer' key={key}>
+          <h1>Random Advices</h1>
+          <div className='advices'>
+            <p>{quote.slip.advice}</p>
+            <button onClick={getQuotes}>Next Advice</button>
+          </div>
+        </div>
+
+      ))}
     </div>
   );
 }
